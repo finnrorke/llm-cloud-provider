1956a696ad4ae115141a82eff2943af6d3908be2
Finn Rorke <finnr@blueyonder.co.uk>
2026-03-03 23:20:20 +0000

    Added dynamo lock table

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,3 +1,12 @@
 resource "aws_s3_bucket" "aws_terraform_state_bucket" {
   bucket = "${var.org_prefix}-terraform-state"
 }
+
+resource "aws_dynamodb_table" "aws_terraform_lock_db" {
+  name = "${var.org_prefix}-terraform-state-lock"
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
